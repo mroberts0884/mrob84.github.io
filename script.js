fbd5853a0f50f7a2c4b2120a2661318caceab496
Matt Roberts <mroberts0884@gmail.com>
2024-08-20 21:35:42 -0400

    added functionality to contact form

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded',
-    function () {
-        const navItems = document.querySelectorAll('.nav-item');
-        
-        navItems.forEach(item => {
-            item.addEventListener('click', function () {
-                    navItems.forEach(this.classList.add('active'));
-                
-                
-                });
-        });
-    });
+/* contact page */
+
+document.getElementById('contact-form').addEventListener('submit', function(e){
+
+    e.preventDefault();
+
+    const formData = new formData(this);
+
+    fetch('form-process.php', {
+        method: 'POST',
+        body: formData
+
+    })
+    .then(response => response.text())
+    .then(
+        data => {document.getElementById('response-message').innerHTML = data;
+})
+    .catch(error => console.error('Error:', error));
+
+})
