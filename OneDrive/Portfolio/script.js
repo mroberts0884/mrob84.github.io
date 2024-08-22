594161182de071fd2f1fbdcb48ec339a37c8d3db
Matt Roberts <mroberts0884@gmail.com>
2024-08-22 00:49:24 -0400

    hope this works

---
+++ OneDrive/Portfolio/script.js
@@ -0,0 +1,20 @@
+/* contact page */
+
+document.getElementById('contact-form').addEventListener('submit', function(e){
+
+    e.preventDefault();
+    
+
+    const formData = new formData(this);
+
+    fetch('https://formspree.io/f/mkgwqveo', {
+        method: 'POST',
+        body: formData
+
+    })
+    .then(response => response.text())
+    .then
+        alert('Form is submitted.')
+    .catch(error => console.error('Error:', error));
+
+})
