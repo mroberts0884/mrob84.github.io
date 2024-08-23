739814deeac831f63a0b1db53b637ab922139aa2
Matt Roberts <mroberts0884@gmail.com>
2024-08-23 01:27:11 -0400

    added reset button with functionality

--- script.js
+++ script.js
@@ -2,19 +2,13 @@
 
 document.getElementById('contact-form').addEventListener('submit', function(e){
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    if (name == "") {
-          alert("Name must be filled out.");
-          return false;
-        }    
-    const email = document.getElementById('emailaddress').value;
-    if (email == "") {
-        alert("Email must be filled out.");
-            return false    
-        }    
-    else {
-            alert("Form submitted. Thank you!")
-            return true    
+   
         }
-      })
+
+        
+      )
+function reset() {
+    document.getElementById("contact-form").reset();
+}
+      
 
