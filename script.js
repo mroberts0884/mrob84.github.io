46e168a9a72e3294300a8b3bba69782560c000be
Matt Roberts <mroberts0884@gmail.com>
2024-08-23 00:05:53 -0400

    fixed merge conflict

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 /* contact page */
 
 document.getElementById('contact-form').addEventListener('submit', function(e){
-<<<<<<< HEAD
     e.preventDefault();
     const name = document.getElementById('name').value;
     if (name == "") {
@@ -19,22 +18,3 @@ document.getElementById('contact-form').addEventListener('submit', function(e){
         }
       })
 
-=======
-
-    e.preventDefault();
-
-    const formData = new formData(this);
-
-    fetch('https://formspree.io/f/mkgwqveo', {
-        method: 'POST',
-        body: formData
-
-    })
-    .then(response => response.text())
-    .then(
-         alert('Form is submitted.')
-)
-    .catch(error => console.error('Error:', error));
-
-})
->>>>>>> 970274a17a45b725612ff6b17ce6d70e210583c6
